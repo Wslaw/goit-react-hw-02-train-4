aae645238c9786da2fd8659ae8174d4a44fda0b5
Wslaw <114644824+Wslaw@users.noreply.github.com>
2024-01-05 17:46:41 +0200

    init

---
+++ src/components/feedback/FeedbackForm.jsx
@@ -0,0 +1,22 @@
+import { Component } from 'react';
+import styles from './feedback.module.css';
+
+
+class FeedbackForm extends Component { }
+
+
+
+
+
+// const FeedbackForm = () => {
+// state = {
+//   good: 0,
+//   neutral: 0,
+//   bad: 0
+// }
+    
+//     render{
+//         <>
+//         </>
+//     }
+// };
